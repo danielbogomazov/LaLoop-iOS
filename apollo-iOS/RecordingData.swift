2920c1c6c5567c74a5066656f81c97be9cf41b5f
danielbogomazov <danielbogomazov@gmail.com>
2019-03-31 01:24:12 -0400

    Closes #30 - store multiple generes per recording

--- apollo-iOS/RecordingData.swift
+++ apollo-iOS/RecordingData.swift
@@ -13,14 +13,18 @@ struct RecordingData: Decodable {
     var recordings: [RecordingObj]
 }
 
+struct GenreObj: Decodable {
+    var genre_id: String
+    var genre_name: String
+}
+
 struct RecordingObj: Decodable {
     var recording_id: String
     var recording_name: String
     var recording_release_date: String
     var artist_id: String
     var artist_name: String
-    var genre_id: String
-    var genre_name: String
+    var genres: [GenreObj]
     var label_id: String
     var label_name: String
     
@@ -28,29 +32,33 @@ struct RecordingObj: Decodable {
 
         recording_name = Util.replaceCharEntitites(string: recording_name)
         artist_name = Util.replaceCharEntitites(string: artist_name)
-        genre_name = Util.replaceCharEntitites(string: genre_name)
         label_name = Util.replaceCharEntitites(string: label_name)
-
-        let recordings = findMatchFor(entity: .recording) as! [Recording]
-        let artists = findMatchFor(entity: .artist) as! [Artist]
-        let genres = findMatchFor(entity: .genre) as! [Genre]
-        let labels = findMatchFor(entity: .label) as! [Label]
+        for var genre in genres {
+            genre.genre_name = Util.replaceCharEntitites(string: genre.genre_name)
+        }
+        
+        let foundRecordings = findMatchFor(entity: .recording) as! [Recording]
+        let foundArtists = findMatchFor(entity: .artist) as! [Artist]
+        let foundGenres = findMatchFor(entity: .genre) as! [Genre]
+        let foundLabels = findMatchFor(entity: .label) as! [Label]
         
         var recording: Recording!
         
-        if recordings.count < 1 {
-            recording = (createNewFor(entity: .recording) as! Recording)
+        if foundRecordings.count < 1 {
+            recording = (createNewFor(entity: .recording)[0] as! Recording)
         } else {
             let formatter = DateFormatter()
             formatter.dateFormat = "yyyy-MM-dd"
-            recording = recordings[0]
+            recording = foundRecordings[0]
             recording.name = recording_name
             recording.release_date = recording_release_date != "" ? formatter.date(from: recording_release_date)! : nil
         }
 
-        artist_id == "" ? recording.artists.removeAll() : addEntityFor(entity: .artist, found: artists, recording: recording)
-        genre_id == "" ? recording.genres.removeAll() : addEntityFor(entity: .genre, found: genres, recording: recording)
-        label_id == "" ? recording.labels.removeAll() : addEntityFor(entity: .label, found: labels, recording: recording)
+        artist_id == "" ? recording.artists.removeAll() : addEntityFor(entity: .artist, found: foundArtists, recording: recording)
+        label_id == "" ? recording.labels.removeAll() : addEntityFor(entity: .label, found: foundLabels, recording: recording)
+        for genre in genres {
+            genre.genre_id == "" ? recording.genres.removeAll() : addEntityFor(entity: .genre, found: foundGenres, recording: recording)
+        }
 
         do {
             try AppDelegate.viewContext.save()
@@ -70,9 +78,9 @@ struct RecordingObj: Decodable {
         case .artist:
             let artists = found as! [Artist]
             if artists.count < 1 {
-                let newEntity = createNewFor(entity: entity) as! Artist
-                newEntity.recordings.insert(recording)
-                recording.artists.insert(newEntity)
+                let newEntities = createNewFor(entity: entity) as! [Artist]
+                newEntities[0].recordings.insert(recording)
+                recording.artists.insert(newEntities[0])
             } else if artists.count == 1 && recording.artists.contains(where: { (artist) -> Bool in return artist.id == artist_id }) {
                 artists[0].name = artist_name
                 recording.artists.insert(artists[0])
@@ -81,20 +89,25 @@ struct RecordingObj: Decodable {
         case .genre:
             let genres = found as! [Genre]
             if genres.count < 1 {
-                let newEntity = createNewFor(entity: entity) as! Genre
-                newEntity.recordings.insert(recording)
-                recording.genres.insert(newEntity)
-            } else if genres.count == 1 && recording.genres.contains(where: { (genre) -> Bool in return genre.id == genre_id }) {
-                genres[0].name = genre_name
-                recording.genres.insert(genres[0])
-                genres[0].recordings.insert(recording)
+                let newEntities = createNewFor(entity: entity) as! [Genre]
+                for newGenre in newEntities {
+                    newGenre.recordings.insert(recording)
+                    recording.genres.insert(newGenre)
+                }
+            } else {
+                for genre in self.genres {
+                    if recording.genres.contains(where: { (g) -> Bool in return g.id == genre.genre_id }) {
+                        recording.genres.insert(genres[0])
+                        genres[0].recordings.insert(recording)
+                    }
+                }
             }
         case .label:
             let labels = found as! [Label]
             if labels.count < 1 {
-                let newEntity = createNewFor(entity: entity) as! Label
-                newEntity.recordings.insert(recording)
-                recording.labels.insert(newEntity)
+                let newEntities = createNewFor(entity: entity) as! [Label]
+                newEntities[0].recordings.insert(recording)
+                recording.labels.insert(newEntities[0])
             } else if labels.count == 1 && recording.labels.contains(where: { (label) -> Bool in return label.id == label_id }) {
                 labels[0].name = label_name
                 recording.labels.insert(labels[0])
@@ -127,14 +140,18 @@ struct RecordingObj: Decodable {
                 return []
             }
         case .genre:
-            let request: NSFetchRequest<Genre> = Genre.fetchRequest()
-            request.predicate = NSPredicate(format: "id == %@", genre_id)
-            do {
-                let found = try AppDelegate.viewContext.fetch(request)
-                return found
-            } catch {
-                return []
+            var foundGenres: [Genre] = []
+            for genre in self.genres {
+                let request: NSFetchRequest<Genre> = Genre.fetchRequest()
+                request.predicate = NSPredicate(format: "id == %@", genre.genre_id)
+                do {
+                    let found = try AppDelegate.viewContext.fetch(request)
+                    foundGenres += found
+                } catch {
+                    return []
+                }
             }
+            return foundGenres
         default:
             let request: NSFetchRequest<Label> = Label.fetchRequest()
             request.predicate = NSPredicate(format: "id == %@", label_id)
@@ -148,7 +165,7 @@ struct RecordingObj: Decodable {
         
     }
     
-    func createNewFor(entity: Util.entity) -> RecordingInformation {
+    func createNewFor(entity: Util.entity) -> [RecordingInformation] {
         switch entity {
         case .recording:
             let newEntity = NSEntityDescription.entity(forEntityName: "Recording", in: AppDelegate.viewContext)!
@@ -158,25 +175,29 @@ struct RecordingObj: Decodable {
             newRecording.id = recording_id
             newRecording.name = recording_name
             newRecording.release_date = recording_release_date != "" ? formatter.date(from: recording_release_date)! : nil
-            return newRecording
+            return [newRecording]
         case .artist:
             let newEntity = NSEntityDescription.entity(forEntityName: "Artist", in: AppDelegate.viewContext)!
             let newArtist = NSManagedObject(entity: newEntity, insertInto: AppDelegate.viewContext) as! Artist
             newArtist.id = artist_id
             newArtist.name = artist_name
-            return newArtist
+            return [newArtist]
         case .genre:
-            let newEntity = NSEntityDescription.entity(forEntityName: "Genre", in: AppDelegate.viewContext)!
-            let newGenre = NSManagedObject(entity: newEntity, insertInto: AppDelegate.viewContext) as! Genre
-            newGenre.id = genre_id
-            newGenre.name = genre_name
-            return newGenre
+            var newGenres: [Genre] = []
+            for genre in genres {
+                let newEntity = NSEntityDescription.entity(forEntityName: "Genre", in: AppDelegate.viewContext)!
+                let newGenre = NSManagedObject(entity: newEntity, insertInto: AppDelegate.viewContext) as! Genre
+                newGenre.id = genre.genre_id
+                newGenre.name = genre.genre_name
+                newGenres.append(newGenre)
+            }
+            return newGenres
         default:
             let newEntity = NSEntityDescription.entity(forEntityName: "Label", in: AppDelegate.viewContext)!
             let newLabel = NSManagedObject(entity: newEntity, insertInto: AppDelegate.viewContext) as! Label
             newLabel.id = label_id
             newLabel.name = label_name
-            return newLabel
+            return [newLabel]
         }
     }
 
