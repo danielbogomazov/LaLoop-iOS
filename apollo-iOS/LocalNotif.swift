a3bf94579407d72375f1c35bfda038450fa934db
danielbogomazov <danielbogomazov@gmail.com>
2019-04-03 23:17:25 -0400

    Closes #33 - Notifications for unknown release dates

--- apollo-iOS/LocalNotif.swift
+++ apollo-iOS/LocalNotif.swift
@@ -14,17 +14,12 @@ struct LocalNotif {
     static let center = UNUserNotificationCenter.current()
     
     static func createNewRecording(recording: Recording, completionHandler: @escaping (Bool, Error?) -> Void) {
-        // TODO : When the date changes, make sure the notification changes as well
-        guard let date = recording.release_date else {
-            completionHandler(false, nil)
-            return
-        }
-        
+
         let content = UNMutableNotificationContent()
         content.title = NSString.localizedUserNotificationString(forKey: "Recording Released!", arguments: nil)
         content.body = createBody(for: recording)
         
-        let dateComponents = Calendar.current.dateComponents([.day, .month, .year], from: date)
+        let dateComponents = Calendar.current.dateComponents([.day, .month, .year], from: recording.release_date ?? getDefaultTBAReleaseDate())
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
         let request = UNNotificationRequest(identifier: recording.id, content: content, trigger: trigger)
         
@@ -45,21 +40,22 @@ struct LocalNotif {
         center.getPendingNotificationRequests(completionHandler: { notif in
             for n in notif {
                 if let recording = AppDelegate.recordings.first(where: { $0.id == n.identifier }) {
-                    if let date = recording.release_date {
-                        if (n.trigger as! UNCalendarNotificationTrigger).dateComponents != Calendar.current.dateComponents([.day, .month, .year], from: date) || n.content.body != createBody(for: recording) {
-                            
-                            removeRecording(id: recording.id)
-                            createNewRecording(recording: recording, completionHandler: { (success, error) in
-                                if let e = error {
-                                    print(e.localizedDescription)
-                                }
-                                if !success {
-                                    // TODO
-                                }
-                            })
-                        }
-                    } else {
-                        // TODO
+                    let date = recording.release_date
+                    let notifDateComponents = (n.trigger as! UNCalendarNotificationTrigger).dateComponents
+                    
+                    if n.content.body != createBody(for: recording) ||
+                        (date == nil && notifDateComponents != Calendar.current.dateComponents([.day, .month, .year], from: getDefaultTBAReleaseDate())) ||
+                        (date != nil && notifDateComponents != Calendar.current.dateComponents([.day, .month, .year], from: date!)) {
+                        
+                        removeRecording(id: recording.id)
+                        createNewRecording(recording: recording, completionHandler: { (success, error) in
+                            if let e = error {
+                                print(e.localizedDescription)
+                            }
+                            if !success {
+                                // TODO
+                            }
+                        })
                     }
                 } else {
                     // TODO
@@ -75,6 +71,12 @@ struct LocalNotif {
             artists.append(contentsOf: name)
         }
         return recording.name == "TBA" ? NSString.localizedUserNotificationString(forKey: "By \(artists)", arguments: nil) : NSString.localizedUserNotificationString(forKey: recording.name + " by \(artists)", arguments: nil)
+    }
+    
+    static func getDefaultTBAReleaseDate() -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter.date(from: "3999/01/01") ?? Date()
     }
 
     private init() {}
